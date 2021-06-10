a08a312f79c4bc830aef627b86d7e00b75913c46
Frizlab <fload@me.com>
2021-06-10 16:42:04 +0200

    Initial Commit

---
+++ Blerk/ViewController.swift
@@ -0,0 +1,23 @@
+/*
+ * ViewController.swift
+ * Blerk
+ *
+ * Created by François Lamboley on 10/06/2021.
+ */
+
+import Cocoa
+
+
+
+class ViewController: NSViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override var representedObject: Any? {
+		didSet {
+		}
+	}
+	
+}
